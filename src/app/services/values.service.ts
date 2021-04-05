9b876b2c8744c15488178b52aca9d982d5c47e3e
Irina Vasilescu <irina>
2021-04-05 21:57:43 +0300

    added some cute bees pngs & some flex

--- src/app/services/values.service.ts
+++ src/app/services/values.service.ts
@@ -23,17 +23,20 @@ export class ValuesService {
         [this.beeTypes.queen]: {
             total: 1,
             hp: 100,
-            damage: 8
+            damage: 8,
+            img: './assets/queen.png'
         },
         [this.beeTypes.worker]: {
             total: 5,
             hp: 75,
-            damage: 10
+            damage: 10,
+            img: './assets/worker.png'
         },
         [this.beeTypes.drone]: {
             total: 8,
             hp: 50,
-            damage: 12
+            damage: 12,
+            img: './assets/drone.png'
         }
     }
 }
