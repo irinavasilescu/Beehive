aa6a6817d2f063c4e5af62d0564160eb841b0ac5
Irina Vasilescu <irina>
2021-04-07 22:12:27 +0300

    test - damageRandomBee

--- src/app/components/game/game.component.spec.ts
+++ src/app/components/game/game.component.spec.ts
@@ -28,20 +28,20 @@ describe('GameComponent', () => {
 
     describe('initBeesState method', () => {
         it('should populate with the right number of bees', () => {
-            let expectedLength = valuesService.beeTypesArray.map(beeType => valuesService.hive[beeType].total)
+            const expectedLength = valuesService.beeTypesArray.map(beeType => valuesService.hive[beeType].total)
                                                             .reduce((acc, val) => acc + val)
             expect(component.bees.length).toBe(expectedLength);
         });
         it('should contain one queen', () => {
-            let containsQueen = component.bees.filter(bee => bee.type === valuesService.beeTypes.queen).length === 1;
+            const containsQueen = component.bees.filter(bee => bee.type === valuesService.beeTypes.queen).length === 1;
             expect(containsQueen).toBeTruthy();
         });
         it('should contain at least one worker', () => {
-            let containsWorker = component.bees.find(bee => bee.type === valuesService.beeTypes.worker) !== undefined;
+            const containsWorker = component.bees.find(bee => bee.type === valuesService.beeTypes.worker) !== undefined;
             expect(containsWorker).toBeTruthy();
         });
         it('should contain at least one drone', () => {
-            let containsDrone = component.bees.find(bee => bee.type === valuesService.beeTypes.drone) !== undefined;
+            const containsDrone = component.bees.find(bee => bee.type === valuesService.beeTypes.drone) !== undefined;
             expect(containsDrone).toBeTruthy();
         });
     });
@@ -49,7 +49,6 @@ describe('GameComponent', () => {
     describe('initVariables method', () => {
         it('should initialize variables', () => {
             component.initVariables();
-            fixture.detectChanges();
             expect(component.gameOver).toBeFalse();
             expect(component.bees).toEqual([]);
             expect(component.damagedBee).toBeUndefined();
@@ -60,7 +59,6 @@ describe('GameComponent', () => {
     describe('initHiveState method', () => {
         it('should initialize hive details', () => {
             component.initHiveState();
-            fixture.detectChanges();
             expect(component.hive.hp).toBeDefined();
             expect(component.hive.total).toBeDefined();
             expect(component.hive.status).toEqual(valuesService.statuses.healthy);
@@ -70,10 +68,26 @@ describe('GameComponent', () => {
     describe('pickRandomBee method', () => {
         it('should return valid index', () => {
             component.bees = [ {hp: 75}, {hp: 100}, {hp: 50}, {hp: 0}, {hp: 75}, {hp: 50}, {hp: 0}, {hp: 100} ]
-            let generatedIndex = component.pickRandomBee();
-            fixture.detectChanges();
-            let isValidIndex = new Set([0, 1, 2, 4, 5, 7]).has(generatedIndex);
+            const generatedIndex = component.pickRandomBee();
+            const isValidIndex = new Set([0, 1, 2, 4, 5, 7]).has(generatedIndex);
             expect(isValidIndex).toBeTrue();
         });
     });
+
+    describe('damageRandomBee method', () => {
+        it('should call pickRandomBee', () => {
+            const spy = spyOn(component, 'pickRandomBee');
+            component.damageRandomBee();
+            expect(spy).toHaveBeenCalled();
+        });
+        it('should call registerDamange', () => {
+            const spy = spyOn(component, 'registerDamage');
+            component.damageRandomBee();
+            expect(spy).toHaveBeenCalled();
+        });
+        it('should write to local storage', () => {
+            component.damageRandomBee();
+            expect(Object.keys(localStorage).length).toBeGreaterThan(0);
+        });
+    })
 });
